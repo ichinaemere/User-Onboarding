faf9ebd8110fceb18c2dbdbe3c0b6018a9b8fcaf
Chinaemere Ike <ms.chinaemere@gmail.com>
2019-11-11 23:58:06 +0000

    MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '/workspace/User-Onboarding/user-onboarding/src/App.css';
+import FormikNewUserForm from './components/Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikNewUserForm />
+    </div>
+  );
+}
+
+export default App;
